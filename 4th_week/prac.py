f467009d648d4bb81b20e22423eed969fc601191
seungheon <seungheon@apexsoft.co.kr>
2025-03-21 20:51:17 +0900

    4주차 선택정렬 결과물

--- 4th_week/prac.py
+++ 4th_week/prac.py
@@ -7,3 +7,13 @@ def bubblesort(nums):
                 nums[i+1] = nums[i]
                 nums[i] = temp
     return nums
+
+def selectionsort(nums):
+    size = len(nums)
+    for j in range(size-1):
+        idx = j
+        for i in range(j+1, size):
+            if nums[idx] > nums[i]:
+                idx = i
+        nums[j], nums[idx] = nums[idx], nums[j]
+    return nums
--- 4th_week/test.py
+++ 4th_week/test.py
@@ -1,4 +1,7 @@
-from prac import bubblesort
+from prac import bubblesort, selectionsort
 
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+assert selectionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert selectionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
