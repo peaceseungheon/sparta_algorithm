e5923a473bd452724d46d018f7c9721d27b12d3d
seungheon <seungheon@apexsoft.co.kr>
2025-03-22 13:20:15 +0900

    4주차 퀵정렬 결과물

--- 4th_week/prac.py
+++ 4th_week/prac.py
@@ -17,3 +17,37 @@ def selectionsort(nums):
                 idx = i
         nums[j], nums[idx] = nums[idx], nums[j]
     return nums
+
+def insertionsort(nums):
+    size = len(nums)
+    if size == 1:
+        return
+
+    for j in range(1, size):
+        for i in range(j, 0, -1):
+            if nums[i] < nums[i-1]:
+                nums[i-1], nums[i] = nums[i], nums[i-1]
+            else:
+                break
+
+    return nums
+
+def quicksort(arr, start, end):
+    if start > end:
+        return
+    # i: pivot 보다 작은 수들의 마지막 index
+    # j: pivot과 비교할 범위
+    pivot = end
+
+    i = start - 1
+
+    for j in range(start, end):
+        # arr[j]가 pivot보다 작으면 i를 1 증가시키고 arr[j]와 arr[i]의 위치를 바꿈
+        if arr[j] < arr[pivot]:
+            i += 1
+            arr[j], arr[i] = arr[i], arr[j]
+
+    arr[pivot], arr[i+1] = arr[i+1], arr[pivot]
+    quicksort(arr, start, i)
+    quicksort(arr, i+2, end)
+    return arr
--- 4th_week/test.py
+++ 4th_week/test.py
@@ -1,7 +1,21 @@
-from prac import bubblesort, selectionsort
+from prac import bubblesort, selectionsort, insertionsort, quicksort
 
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
 
 assert selectionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert selectionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+assert insertionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert insertionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+assert quicksort([4, 6, 2, 9, 1], 0, 1) == [4, 6, 2, 9, 1]
+assert quicksort([4, 6, 2, 9, 1], 0, 2) == [2, 4, 6, 9, 1]
+assert quicksort([4, 6, 2, 9, 1], 0, 3) == [2, 4, 6, 9, 1]
+assert quicksort([4, 6, 2, 9, 1], 0, 4) == [1, 2, 4, 6, 9]
+assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 1) == [2, 3, 1, 5, 3, 2, 3]
+assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 2) == [1, 2, 3, 5, 3, 2, 3]
+assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 3) == [1, 2, 3, 5, 3, 2, 3]
+assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 4) == [1, 2, 3, 3, 5, 2, 3]
+assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 5) == [1, 2, 2, 3, 3, 5, 3]
+assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 6) == [1, 2, 2, 3, 3, 3, 5]
